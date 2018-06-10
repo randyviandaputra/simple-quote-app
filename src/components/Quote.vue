3809402efa3ef73a2cf071d2b821e837b30ae7b7
Randy Vianda Putra <randy@docotel.com>
2018-06-10 14:30:19 +0700

    first commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-sm-12 col-md-12 col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+    .panel-body {
+        font-size: 24px;
+        font-style: italic;
+        color: #6e6e6e;
+    }
+
+    .quote {
+        cursor: pointer;
+    }
+
+    .quote:hover {
+        background-color: #ffe2e2;
+    }
+</style>
